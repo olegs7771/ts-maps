a95e088a2a95bd91fb29c5838032ccf8e7b09a60
olegs7771 <olegs7771@gmail.com>
2021-04-08 13:06:19 +0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 // const user = new User();
 // const company = new Company();
 // console.log(user);
 // console.log(company);
-const map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+
+const user = new User();
+const company = new Company();
+const map = new CustomMap('map');
+map.addMarker(user);
+map.addMarker(company);
