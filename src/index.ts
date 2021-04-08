fb775546782c5bbac0c68d6123d870af63fdf653
olegs7771 <olegs7771@gmail.com>
2021-04-08 08:30:02 +0300

    ok

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// import { User } from './User';
+// import { Company } from './Company';
+
+// const user = new User();
+// const company = new Company();
+// console.log(user);
+// console.log(company);
+const map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
